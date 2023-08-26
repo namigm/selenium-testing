c6c4ad334d5f160270e9df08cd9b21165f6adc00
Namig <masimovnamik@gmail.com>
2023-08-26 14:51:22 +0200

    .env and env_creds configuration

--- env_setup.py
+++ env_setup.py
@@ -1,4 +1,8 @@
-BASE_URL = "https://toghrulmirzayev.github.io/ui-simulator"
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+BASE_URL = os.getenv('BASE_URL')
 CHECK_VALIDATE_URL = f"{BASE_URL}/check_and_validate.html"
 INPUT_CLICK_URL = f"{BASE_URL}/input-and-click.html"
 
