fd234da3b705f2064d27e90acf2c232808045b5e
Namig <masimovnamik@gmail.com>
2023-08-26 15:54:54 +0200

    added username/password to env_setup

--- env_setup.py
+++ env_setup.py
@@ -5,6 +5,9 @@ load_dotenv()
 BASE_URL = os.getenv('BASE_URL')
 CHECK_VALIDATE_URL = f"{BASE_URL}/check_and_validate.html"
 INPUT_CLICK_URL = f"{BASE_URL}/input-and-click.html"
+WAPP_USER_NAME = os.getenv("WAPP_USER_NAME")
+WAPP_PASSWORD = os.getenv("WAPP_PASSWORD")
+
 
 
 
